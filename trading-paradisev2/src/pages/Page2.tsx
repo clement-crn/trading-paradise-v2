2bb5c2cd196376ebade975781bed22ed4797eeeb
clement <86530475+clementciron@users.noreply.github.com>
2022-08-31 19:01:38 +0200

    body fixed

--- trading-paradisev2/src/pages/Page2.tsx
+++ trading-paradisev2/src/pages/Page2.tsx
@@ -2,13 +2,16 @@
 import './Page2.css'
 import Graph from '../components/Graph/Graph'
 import FloatingPanel from '../components/Floatingpanel/FloatingPanel'
-
+import Navbar from '../components/Navbar';
+import Ticker from '../components/ticker';
 
 
 export default function Page2(){
     return(
         
         <div className='page2-container'>
+            <Navbar/>
+        <Ticker />
             <div className='graph-title'>
                 Entrez dans le nouveau monde.
             </div>
