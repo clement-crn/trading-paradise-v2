458b3c8b64d377dede249f9a8add89a690844a4d
Clément <cironclement.pro@gmail.com>
2022-09-19 22:46:23 +0200

    correction responsive navbar

--- trading-paradisev2/src/pages/Home.jsx
+++ trading-paradisev2/src/pages/Home.jsx
@@ -1,23 +1,20 @@
-import './home.css';
-import videoBg from '../components/videobg.mp4'
-import Navbar from '../components/Navbar';
-import Ticker from '../components/ticker';
-import { Link } from 'react-router-dom'
- function Home(){
-    return(
-    
-        <div className='home'>
-        <Navbar/>
-        <Ticker />
-        
-        <div className="content">
-        
-            <h1>Commencez votre aventure</h1>
-            <Link to='/page2'>Nous rejoindre</Link>
-            <video src={videoBg} autoPlay loop muted />
+import "./home.css";
+import videoBg from "../components/videobg.mp4";
+import Navbar from "../components/Navbar";
+import Ticker from "../components/ticker";
+import { Link } from "react-router-dom";
+function Home() {
+    return (
+        <div className="home">
+            <Navbar />
+            <Ticker />
+
+            <div className="content">
+                <h1>Commencez votre aventure</h1>
+                <Link to="/page2">Nous rejoindre</Link>
+                <video muted playsinline src={videoBg} autoPlay loop muted />
+            </div>
         </div>
-        </div>
-        
-    )
+    );
 }
-export default Home
+export default Home;
