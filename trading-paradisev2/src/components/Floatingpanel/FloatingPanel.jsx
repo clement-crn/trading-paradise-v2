b1c73405986a04d7b66a638bd0a2368c65440b49
Clément <cironclement.pro@gmail.com>
2022-09-20 00:32:06 +0200

    correction responsive navbar

--- trading-paradisev2/src/components/Floatingpanel/FloatingPanel.jsx
+++ trading-paradisev2/src/components/Floatingpanel/FloatingPanel.jsx
@@ -1,56 +1,56 @@
-import './floatingpanel.scss'
+import "./floatingpanel.scss";
 
 function FloatingPanels() {
-
-    return(
-
+    return (
         <div>
+            <div class="v-center">
+                <article>
+                    <div class="front">
+                        <div class="text">
+                            <h1>Placements</h1>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="text">
+                            <p>
+                                Actions, ETF, produits dérivés, vos décisions
+                                n'ont aucune limite.
+                            </p>
+                        </div>
+                    </div>
+                </article>
 
-        <div class="v-center">
-   <article>
-    <div class="front">
-      <div class="text">
-        <h1>Placements</h1>
-      </div>
-    </div>
-    <div class="back">
-      <div class="text">
-        <p>Actions, ETF, produits dérivés, vos décisions n'ont aucune limite.</p>
-      </div>
-    </div>
-  </article>
-  
-  <article>
-    <div class="front">
-      <div class="text">
-        <h1>Organisation</h1>
-      </div>
-    </div>
-    <div class="back">
-      <div class="text">
-        <p>Suivez vos objectifs sur ordinateur, téléphone ou tablette.</p>
-      </div>
-    </div>
-  </article>
-  
-  <article>
-    <div class="front">
-      <div class="text">
-        <h1>Support</h1>
-      </div>
-    </div>
-    <div class="back">
-      <div class="text">
-        <p>Support disponible 24/7.</p>
-      </div>
-    </div>
-  </article>
+                <article>
+                    <div class="front">
+                        <div class="text">
+                            <h1>Organisation</h1>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="text">
+                            <p>
+                                Suivez vos objectifs sur ordinateur, téléphone
+                                ou tablette.
+                            </p>
+                        </div>
+                    </div>
+                </article>
 
-</div>
-
-<p class="auth"><a href="http://blog.devolk.pl" taget="_blank">devolk.pl</a></p>
-</div>
-    )
+                <article>
+                    <div class="front">
+                        <div class="text">
+                            <h1>Support</h1>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="text">
+                            <p>Support disponible 24/7.</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    );
 }
 
-export default FloatingPanels
+export default FloatingPanels;
